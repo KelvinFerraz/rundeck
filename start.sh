b1b32cd3dab20ffba00f11a8e70dede12048bc8d
Ferraz <kelvinferrazsilva@gmail.com>
2020-05-10 15:14:23 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-# Get Public IP 
-HOSTNAME_RUNDECK=$(curl ifconfig.me)
 
-# SHARE Public IP Geted to env file
-echo "HOSTNAME_RUNDECK=${HOSTNAME_RUNDECK}" >> .env
+# Variables
+EXIST_HOSTNAME=$(cat .env | grep -o HOSTNAME_RUNDECK)
 
 
-# Exec compose
-docker-compose up -d
+if [ -z $EXIST_HOSTNAME ];then
+    # # Exec compose
+    docker-compose up -d
+else
+    # # Get Public IP 
+    HOSTNAME_RUNDECK=$(curl ifconfig.me)
+
+    # # SHARE Public IP Geted to env file
+    echo "HOSTNAME_RUNDECK=${HOSTNAME_RUNDECK}" >> .env
+
+    # # Exec compose
+    docker-compose up -d
+fi
