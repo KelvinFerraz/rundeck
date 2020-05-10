d008e33c0bbca32a74f4eb810b6e2c7a4caaa932
Ferraz <kelvinferrazsilva@gmail.com>
2020-05-10 15:14:36 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ else
     HOSTNAME_RUNDECK=$(curl ifconfig.me)
 
     # # SHARE Public IP Geted to env file
-    echo "HOSTNAME_RUNDECK=${HOSTNAME_RUNDECK}" >> .env
+    echo "HOSTNAME_RUNDECK=${HOSTNAME_RUNDECK}" > .env
 
     # # Exec compose
     docker-compose up -d
